a32fbde3b2699df52d79f087b22e2471c315332c
yohan99x <yohanj@99x.io>
2022-08-31 21:21:04 +0530

    logging added

---
+++ net-interviewing-project-v2/src/Insurance.Api/Controllers/InsuranceController.cs
@@ -0,0 +1,28 @@
+﻿using Insurance.Api.Models.Insurance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Insurance.Api.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        private readonly ILogger<InsuranceController> _logger;
+
+        public InsuranceController(ILogger<InsuranceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/insurance/products/{productId}")]
+        public async Task<ProductInsuranceResponse> CalculateInsuranceForProduct([FromRoute] int productId)
+        {
+            return new ProductInsuranceResponse()
+            {
+
+            };
+        }
+    }
+}
---
+++ net-interviewing-project-v2/src/Insurance.Api/Models/Insurance/ProductInsuranceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Insurance.Api.Models.Insurance
+{
+    public class ProductInsuranceResponse
+    {
+        public int ProductId { get; set; }
+
+        public int InsuranceValue { get; set; }
+    }
+}
